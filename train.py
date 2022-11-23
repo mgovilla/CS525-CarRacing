cf63f3ba1459b91a7a92a4177ac753f7d116a482
Ian Coolidge <iecoolidge@wpi.edu>
2022-11-22 20:31:24 -0500

    part 2

--- train.py
+++ train.py
@@ -16,7 +16,7 @@ def parse():
 if __name__ == '__main__':
     args = parse()
     start_time = time.time()
-    env = gym.make("CarRacing-v2", continuous=False)
+    env = make_wrap_box2d()
     from models.agent_dqn import Agent_DQN
     agent = Agent_DQN(env, args)
     agent.train()
